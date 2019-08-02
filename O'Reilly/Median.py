91b204eebae57a5602432b1df2c50e6b019ea36c
LittleTB <pengyouwei1997@gmail.com>
2019-08-02 16:50:25 +0800

    add comment

--- O'Reilly/Median.py
+++ O'Reilly/Median.py
@@ -2,7 +2,7 @@ def median(data: list) -> [int, float]:
     data = sorted(data)
     x = len(data)
     if x % 2 == 0:
-        result = (data[int(x/2)] + data[int(x/2-1)]) * 0.5
+        result = (data[int(x/2)] + data[int(x/2-1)]) * 0.5 # Python3中：/ 得到浮点数，// 得到的是整数（地板除）
     else:
         result = data[int((x-1)/2)]
     return result

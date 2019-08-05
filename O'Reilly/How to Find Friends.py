9d39be09882172bdd6d9010d8f8cf819c323c8aa
LittleTB <pengyouwei1997@gmail.com>
2019-08-05 14:34:33 +0800

    delete space line

--- O'Reilly/How to Find Friends.py
+++ O'Reilly/How to Find Friends.py
@@ -9,7 +9,6 @@ def check_connection(network, first, second):
 
     return second in team
 
-
 print(check_connection(
 	("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
 	 "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super"),
@@ -18,3 +17,4 @@ print(check_connection(
 	("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
 	 "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super"),
 	"dr101", "sscout"))
+    
